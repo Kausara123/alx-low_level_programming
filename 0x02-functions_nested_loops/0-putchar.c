3dcaaba79673cd60b431910a671cda220ea8ebfb
Dauda Kausara Temilade <kausaradauda@gmail.com>
2022-09-13 10:48:09 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,28 +1,16 @@
 #include <stdio.h>
 
-
-
 /**
  *
- *  * main - Entry point
+ * main - Entry point
+ * Description: 'the program's description'
  *
- *   * 
- *
- *    * Description: 'the program's description'
- *
- *     * 
- *
- *      * Return: Always 0 (Success)
- *
- *       */
-
-
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
 {
-
-	    printf("_putchar");
-	    return (0);
+	printf("_putchar")
+		    return (0);
 
 }
