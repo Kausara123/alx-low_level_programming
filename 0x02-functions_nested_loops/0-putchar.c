890d63f131c78a803c876bdc71761187f41bf8cd
Dauda Kausara Temilade <kausaradauda@gmail.com>
2022-09-13 10:41:43 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 
-/*
- * Function nested loop
- */
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * 
+ *
+ *    * Description: 'the program's description'
+ *
+ *     * 
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
+
+
 
 int main(void)
+
 {
-	printf("_putchar");
-	return (0);
+
+	    printf("_putchar");
+	    return (0);
+
 }
