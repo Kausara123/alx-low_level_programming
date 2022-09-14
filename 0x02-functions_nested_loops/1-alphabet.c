e5864ee5f12de35f43f8b8e81d347d60c9eb8a91
Dauda Kausara Temilade <kausaradauda@gmail.com>
2022-09-14 11:34:06 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,33 +1,16 @@
-#include <unistd.h>
+#include "main.h"
 
-void ft_putchar(char c) { write(STDOUT_FILENO, &c, 1); }
-
-
-
-void ft_print_alphabet(void) {
-
-	  char letter;
-
-
-
-	    letter = 'a';
-
-	      while (letter <= 'z') {
-
-		          ft_putchar(letter);
-
-			      letter++;
-
-			        }
-
-}
-
-
-
-int main(void) {
-
-	  ft_print_alphabet();
-
-	    return 0;
-
+/**
+ *
+ * main - check the code
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int letter;
+	for(letter='a'; letter<='z'; letter++)
+	{
+		putchar(letter);
+	        return (0);
+	}
 }
